7f26e50b573289af81713e95c95bb28f3210d5fa
andré <andredds12@gmail.com>
2020-08-13 22:55:12 -0300

    comment api path

--- src/services/countryService.js
+++ src/services/countryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = "https://restcountries.eu/rest/v2";
-const API_SEARCH_NAME = "/name";
+// const API_SEARCH_NAME = "/name";
 
 export const listAll = () => axios.get(API_BASE_URL);
