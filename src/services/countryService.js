18c6f3bbb6c363eea10190584b04a180c97ad9db
André Dias da Silva <50876496+diasandre@users.noreply.github.com>
2022-11-01 22:19:12 -0300

    fix url

--- src/services/countryService.js
+++ src/services/countryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://restcountries.eu/rest/v2";
+const API_BASE_URL = "https://restcountries.com/v2/all";
 // const API_SEARCH_NAME = "/name";
 
 export const listAll = () => axios.get(API_BASE_URL);
